919bac72054029c3ccfec0fd89a73d3876ccf007
Eduardo <Eduardo.salinas16@utrgv.edu>
2023-11-29 16:32:04 -0600

    Added front-end code

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-# First Commit
-print("Hello World")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+app.run()
+debug = True
